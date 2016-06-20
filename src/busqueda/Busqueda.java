4207e6e2cc4d3008bf9a0c4343a809aa526d3821
epastorizalorenzo <epastorizalorenzo@atanasoff26.danielcastelao.org>
2016-06-20 10:36:55 +0200

    damian

--- src/busqueda/Busqueda.java
+++ src/busqueda/Busqueda.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class Busqueda {
-
+//damian
     public static void main(String[] args) {
         //unida la rama grafica a la master
         int tamaño;
